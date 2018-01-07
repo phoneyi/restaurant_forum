7a53ac444c2c5614749ed5ded1338c81e370c5cf
phoneyi <yurthebest@gmail.com>
2018-01-07 12:25:38 +0800

    add restaurant destroy action for admin

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -7,7 +7,7 @@ class Admin::RestaurantsController < ApplicationController
 
   before_action :authenticate_user!
   before_action :authenticate_admin
-  before_action :set_restaurant, only: [:show, :edit, :update]
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
 
 
 
@@ -36,6 +36,11 @@ class Admin::RestaurantsController < ApplicationController
     end
   end
 
+  def destroy
+    @restaurant.destroy
+    redirect_to admin_restaurants_path
+    flash[:alert] = "restaurant was deleted"
+  end
 
 
   private
